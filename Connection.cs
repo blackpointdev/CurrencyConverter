f290a2ddfbcf5158de7012a059a71340d4fb395e
blackpointdev <marcinw1998@gmail.com>
2019-10-16 20:28:11 +0200

    Initial commit.

---
+++ Connection.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Xml;
+using System.Net;
+
+namespace CurrencyConverter
+{
+    class Connection
+    {
+        public string Url { get; private set; }
+
+        public Connection(string Url)
+        {
+            this.Url = Url;
+        }
+
+        public XmlDocument GetData()
+        {
+            string xmlStr;
+            using(var wc = new WebClient())
+            {
+                xmlStr = wc.DownloadString(Url);
+            }
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlStr);
+
+            return xmlDoc;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connection connection = new Connection("http://www.nbp.pl/kursy/xml/LastA.xml");
+            Console.WriteLine(connection.GetData().InnerXml);
+        }
+    }
+}
