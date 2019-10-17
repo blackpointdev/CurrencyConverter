dadbfde9905f8dc34bdea64b812eb8c5185a50fa
blackpointdev <marcinw1998@gmail.com>
2019-10-17 12:26:07 +0200

    Added fixed and working ParserXml.

--- CurrencyConverter/ParserXml.cs
+++ CurrencyConverter/ParserXml.cs
@@ -4,14 +4,32 @@ using System.Xml;
 
 public class ParserXml : IParser
 {
-    private XmlDocument xmlDocument { get; set; }
+    private XmlDocument xmlDoc { get; set; }
 
-    public ParserXml(XmlDocument xmlDocument)
+    public ParserXml(XmlDocument xmlDoc)
     {
-        this.xmlDocument = xmlDocument;
+        this.xmlDoc = xmlDoc;
     }
     public void Parse()
     {
-        Console.WriteLine(xmlDocument.OuterXml);
+        // Console.WriteLine(xmlDocument.OuterXml);
+        Console.WriteLine("Parsing...");
+        
+        // XmlNode node = xmlDoc.SelectSingleNode("tabela_kursow/pozycja/nazwa_waluty");
+        
+        // foreach(XmlNode node in xmlDoc.DocumentElement.ChildNodes)
+        // {
+        //     XmlNode innerNode = node.SelectSingleNode("nazwa_waluty");
+        //     Console.WriteLine(innerNode.InnerText);
+        // }
+
+        XmlNodeList nodes = xmlDoc.SelectNodes("tabela_kursow/pozycja");
+
+        foreach(XmlNode node in nodes)
+        {
+            string txt = node.SelectSingleNode("nazwa_waluty").InnerText;
+            Console.WriteLine(txt);
+            // TODO create Currency object!
+        }
     }
 }
