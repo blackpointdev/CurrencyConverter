620c4eb9116dbd2e696cfb3afb641db374506062
blackpointdev <marcinw1998@gmail.com>
2019-10-18 19:15:05 +0200

    Minor fixes.

--- src/CurrencyConverter.UI/Program.cs
+++ src/CurrencyConverter.UI/Program.cs
@@ -8,10 +8,9 @@ namespace CurrencyConverter.UI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Connectiong with: http://www.nbp.pl/kursy/xml/LastA.xml");
-
             // Making HTTP request and downloading XML file.
             var connection = new Connection("http://www.nbp.pl/kursy/xml/LastA.xml");
+            Console.WriteLine("Connectiong with: {0}", connection.Url);
             // Parsing XML file.
             var parserXml = new ParserXml(connection.GetResource());
             var currencyList = parserXml.Parse();
