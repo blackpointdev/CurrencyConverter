a0dd034e691d9bd66083005aab410d507815f485
blackpointdev <marcinw1998@gmail.com>
2019-10-18 19:45:35 +0200

    Structure refactoring, again.

--- src/CurrencyConverter.UI/Menu.cs
+++ src/CurrencyConverter.UI/Menu.cs
@@ -4,18 +4,19 @@ using CurrencyConverter.Core;
 
 namespace CurrencyConverter.UI
 {
-    class Program
+    public class Menu
     {
-        static void Main(string[] args)
+        // Making HTTP request and downloading XML file.
+        public void Run()
         {
-            // Making HTTP request and downloading XML file.
             var connection = new Connection("http://www.nbp.pl/kursy/xml/LastA.xml");
             Console.WriteLine("Connectiong with: {0}", connection.Url);
+            
             // Parsing XML file.
             var parserXml = new ParserXml(connection.GetResource());
             var currencyList = parserXml.Parse();
-
             Console.WriteLine(Converter.Convert(currencyList[0], currencyList[1], 20));
         }
+
     }
 }
---
+++ src/CurrencyConverter/EntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CurrencyConverter.UI;
+
+namespace CurrencyConverter
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Run();
+        }
+    }
+}
